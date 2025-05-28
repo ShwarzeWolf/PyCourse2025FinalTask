4e96e3414d9cf8d8e21c5351448e1d47a8f711e8
Apollon1243 <Stockvstoys@gmail.com>
2025-05-28 12:57:33 +0500

    Upgrade help function and started loading function

--- smilesbot.py
+++ smilesbot.py
@@ -1,7 +1,9 @@
 import os
+import io
 
 from dotenv import load_dotenv
 from telebot import TeleBot
+import pandas as pd
 
 import smiles_info
 
@@ -10,13 +12,15 @@ TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 
 bot = TeleBot(TG_BOT_TOKEN)
 
+with open("text_titles.txt", "r") as file:
+    data = file.readlines()
 
 @bot.message_handler(commands=["start"])
 def greet(message):
     print(message)
     bot.send_message(
         message.chat.id,
-        "Hi, i\'m smiles bot",
+        data[0],
     )
 
 @bot.message_handler(commands=["work"])
@@ -25,22 +29,46 @@ def work(message):
     info = smiles_info.get_molecule_properties(message)
     bot.send_message(
         message.chat.id,
-        f"Here some information about {message}:\nMolecular wight = {info["molecular_weight"]}\nCount of ring = {info["ring_count"]}\nCount of hydrogen bond donors = {info["h_donors"]}\nCount of hydrogen bond acceptors = {info["h_acceptors"]}\nWhether Lipinski rule = {info["lipinski_compliant"]}\n"
+        data[1]
     )
     bot.send_photo(
         message.chat.id, 
         open(info['icon'], 'rb')
     )
 
+@bot.message_handler(commands=["upload"])
+def handle_csv(message):
+    try:
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        csv_data = io.StringIO(downloaded_file.decode('utf-8'))
+        df = pd.read_csv(csv_data)
+    except:
+        bot.send_message(
+            message.chat.id,
+            data[9]
+        )
+        raise ValueError("incorrect file type")
 
 @bot.message_handler(commands=["help"])
 def get_help(message):
     print(message)
+    markup = TeleBot.types.ReplyKeyboardMarkup(row_width=2)
+    btn1 = TeleBot.types.KeyboardButton(data[6], callback_data="button1")
+    btn2 = TeleBot.types.KeyboardButton(data[7], callback_data="button2")
+    markup.add(btn1, btn2)
     bot.send_message(
         message.chat.id,
-        "I can give you some information about organic compound:\n\t-Molecular wight\n\t-Count of ring\n\t-Count of hydrogen bond donors\n\t-Count of hydrogen bond acceptors\n\t-Whether Lipinski rule\n\t-picture of organic compound",
-        # TODO Добавить более точное описание функционала
+        data[2],
+        reply_markup=markup
     )
+# TODO Добавить более точное описание функционала
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    if call.data == "button1":
+        bot.answer_callback_query(call.id, data[3])
+    elif call.data == "button2":
+        bot.answer_callback_query(call.id, data[4])
 
 bot.polling()
