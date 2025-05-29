1ddbd0485df03fa32eb40ccb26491b4e1afefcf8
Apollon1243 <Stockvstoys@gmail.com>
2025-05-29 12:13:51 +0500

    PEP-8 and text out

--- smilesbot.py
+++ smilesbot.py
@@ -1,5 +1,6 @@
 import os
 import io
+import json
 
 from dotenv import load_dotenv
 from telebot import TeleBot
@@ -7,34 +8,39 @@ import pandas as pd
 
 import smiles_info
 
+
 load_dotenv()
 TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 
 bot = TeleBot(TG_BOT_TOKEN)
 
-with open("text_titles.txt", "r") as file:
-    data = file.readlines()
+
+with open('text_titles.json', 'r', encoding='utf-8') as f:
+    Text_for_message = json.load(f)
+
 
 @bot.message_handler(commands=["start"])
 def greet(message):
     print(message)
     bot.send_message(
         message.chat.id,
-        data[0],
+        Text_for_message['welcome'],
     )
 
+
 @bot.message_handler(commands=["work"])
-def work(message):
+def send_smiles_information(message):
     print(message)
     info = smiles_info.get_molecule_properties(message)
     bot.send_message(
         message.chat.id,
-        data[1]
+        Text_for_message['send_info_smiles']
     )
     bot.send_photo(
         message.chat.id, 
         open(info['icon'], 'rb')
     )
+
 
 @bot.message_handler(commands=["upload"])
 def handle_csv(message):
@@ -46,11 +52,34 @@ def handle_csv(message):
     except:
         bot.send_message(
             message.chat.id,
-            data[9]
+            Text_for_message["type_error"]
         )
         raise ValueError("incorrect file type")
 
+
 @bot.message_handler(commands=["help"])
+def get_help(message):
+    print(message)
+    markup = TeleBot.types.ReplyKeyboardMarkup(row_width=2)
+    btn1 = TeleBot.types.KeyboardButton(Text_for_message["help_button_info"], callback_data="help_button_info")
+    btn2 = TeleBot.types.KeyboardButton(Text_for_message["help_button_similar"], callback_data="help_button_similar")
+    markup.add(btn1, btn2)
+    bot.send_message(
+        message.chat.id,
+        Text_for_message["HELP_QUESTION"],
+        reply_markup=markup
+    )
+# TODO Добавить более точное описание функционала
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    if call.data == "help_button_info":
+        bot.answer_callback_query(call.id, Text_for_message["info_about_info_search"])
+    elif call.data == "help_button_similar":
+        bot.answer_callback_query(call.id, Text_for_message["info_about_similar_search"])
+
+bot.polling()
 def get_help(message):
     print(message)
     markup = TeleBot.types.ReplyKeyboardMarkup(row_width=2)
